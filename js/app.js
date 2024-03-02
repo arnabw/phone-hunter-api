f6435adb594e303dd4d08a7c83e2408d08c0ce5d
arnabw <arnabsahawrk@gmail.com>
2024-03-02 14:09:46 +0600

    modall added

--- js/app.js
+++ js/app.js
@@ -41,8 +41,11 @@ const displayPhones = (phones, isShowAll = false) => {
   </div>
   <h3 class="text-2xl font-bold text-[#403F3F]">${phone.phone_name}</h3>
   <button
+  data-modal-target="static-modal"
+  data-modal-toggle="static-modal"
     class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-4 py-2"
     type="button"
+    onclick="showDetails('${phone.slug}')"
   >
     Show Details
   </button>
@@ -75,3 +78,54 @@ const spinnerToggle = (isLoading) => {
 const showAll = () => searchPhone(true);
 
 loadData();
+
+// Show Details Button Click
+async function showDetails(id) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  modal(data.data);
+}
+
+function modal(phoneDetail) {
+  // console.log(phoneDetail);
+  const modalShow = document.getElementById("modalBody");
+
+  modalShow.innerHTML = `
+  <div class="bg-[#0D6EFD0D] rounded-lg flex justify-center py-2 md:py-4">
+  <img
+    class="max-w-48 md:max-w-56 object-cover"
+    src="${phoneDetail.image}"
+    alt=""
+  />
+</div>
+<h3 class="text-lg md:text-2xl font-bold text-[#403F3F]">${
+    phoneDetail.name
+  }</h3>
+<ul class="space-y-4">
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">Storage : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.mainFeatures?.storage || "Not Available"
+  }</span></li>
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">Display Size : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.mainFeatures?.displaySize || "Not Available"
+  }</span></li>
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">Chipset : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.mainFeatures?.chipSet || "Not Available"
+  }</span></li>
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">Memory : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.mainFeatures?.memory || "Not Available"
+  }</span></li>
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">Release data : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.releaseDate || "Not Available"
+  }</span></li>
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">Brand : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.brand || "Not Available"
+  }</span></li>
+  <li class="text-[#403F3F] text-base md:text-xl font-bold">GPS : <span class="text-[##706F6F] font-normal">${
+    phoneDetail?.others?.GPS || "Not Available"
+  }</span></li>
+</ul>
+
+  `;
+}
